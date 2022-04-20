1d1147d29af8e99b7556a6363d96ebe5b79faebf
fanzhuo <fans068@163.com>
2022-04-21 04:14:22 +0800

    配置方案的实现

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/fans1992/gohub
 
 go 1.16
 
-require github.com/gin-gonic/gin v1.7.7
+require (
+	github.com/gin-gonic/gin v1.7.7
+	github.com/spf13/cast v1.4.1
+	github.com/spf13/viper v1.11.0
+)
--- main.go
+++ main.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/fans1992/gohub/bootstrap"
+	btsConfig "github.com/fans1992/gohub/config"
+	"github.com/fans1992/gohub/pkg/config"
+
 	"github.com/gin-gonic/gin"
 )
 
+func init() {
+	// 加载 config 目录下的配置信息
+	btsConfig.Initialize()
+}
+
 func main() {
+
+	// 配置初始化，依赖命令行 --env 参数
+	var env string
+	flag.StringVar(&env, "env", "", "加载 .env 文件，如 --env=testing 加载的是 .env.testing 文件")
+	flag.Parse()
+	config.InitConfig(env)
 
 	// new 一个 Gin Engine 实例
 	router := gin.New()
@@ -14,8 +29,8 @@ func main() {
 	// 初始化路由绑定
 	bootstrap.SetupRoute(router)
 
-	// 运行服务，默认为 8080，我们指定端口为 9999
-	err := router.Run(":9999")
+	// 运行服务
+	err := router.Run(":" + config.Get("app.port"))
 	if err != nil {
 		// 错误处理，端口被占用了或者其他错误
 		fmt.Println(err.Error())
